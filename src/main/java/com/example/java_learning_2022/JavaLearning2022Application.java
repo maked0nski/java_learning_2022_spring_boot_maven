e8a51a9fd2ee8b725ea494abaa0666779d6d52fc
maked0nski <maked0nskihtc@gmail.com>
2022-03-23 00:28:21 +0200

    Initial commit spring boot maven

---
+++ src/main/java/com/example/java_learning_2022/JavaLearning2022Application.java
@@ -0,0 +1,13 @@
+package com.example.java_learning_2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLearning2022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaLearning2022Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/java_learning_2022/JavaLearning2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_learning_2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLearning2022ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
