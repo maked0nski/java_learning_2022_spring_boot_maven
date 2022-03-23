e54e8bf235e1f234e9aa01a7fad8c65561af2800
maked0nski <maked0nskihtc@gmail.com>
2022-03-23 21:54:31 +0200

    homework_1

--- src/main/java/com/example/java_learning_2022/controllers/MainController.java
+++ src/main/java/com/example/java_learning_2022/controllers/MainController.java
@@ -14,58 +14,57 @@ public class MainController {
     List<Post> posts = new ArrayList<>();
 
 
-    public MainController(){
-        users.add(new User(2,"kokos", true));
-        users.add(new User(1,"ananas", true));
-        users.add(new User(3,"banana", false));
-        users.add(new User(4,"tomat", true));
-        users.add(new User(5,"mango", false));
-        users.add(new User(6,"lime", false));
+    public MainController() {
+        users.add(new User(2, "kokos", true));
+        users.add(new User(1, "ananas", true));
+        users.add(new User(3, "banana", false));
+        users.add(new User(4, "tomat", true));
+        users.add(new User(5, "mango", false));
+        users.add(new User(6, "lime", false));
 
-        posts.add(new Post(1,"title1", "body1", 1));
-        posts.add(new Post(2,"title2", "body2", 2));
-        posts.add(new Post(3,"title3", "body3", 3));
-        posts.add(new Post(4,"title4", "body4", 4));
-        posts.add(new Post(5,"title5", "body5", 5));
-        posts.add(new Post(6,"title6", "body6", 6));
+        posts.add(new Post(1, "title1", "body1", 1));
+        posts.add(new Post(2, "title2", "body2", 2));
+        posts.add(new Post(3, "title3", "body3", 3));
+        posts.add(new Post(4, "title4", "body4", 4));
+        posts.add(new Post(5, "title5", "body5", 5));
+        posts.add(new Post(6, "title6", "body6", 6));
 
     }
 
 
-
     @GetMapping("/")
-    public String welcome(){
+    public String welcome() {
         return "Welcome";
     }
 
     @GetMapping("/users")
-    public List<User> users(){
+    public List<User> users() {
         return users;
     }
 
     @GetMapping("/users/{id}")
-    public User user(@PathVariable("id") int id){
-        return users.stream().filter(user -> user.getId()==id).findFirst().get();
+    public User user(@PathVariable("id") int id) {
+        return users.stream().filter(user -> user.getId() == id).findFirst().get();
     }
 
     @GetMapping("/user")
-    public User getUserByParams(@RequestParam("userId") int userId ) {
-        return users.stream().filter(user -> user.getId()==userId).findFirst().get();
+    public User getUserByParams(@RequestParam("userId") int userId) {
+        return users.stream().filter(user -> user.getId() == userId).findFirst().get();
     }
 
     @PostMapping("/user")
     public List<User> saveUser(@RequestBody User user) {
         System.out.println(user);
-        if (user!= null){
+        if (user != null) {
             users.add(user);
-        }else {
+        } else {
             throw new RuntimeException("no user");
         }
         return users;
     }
 
     @PutMapping("/users/{id}")
-    public List<User> putUser(@PathVariable int id, @RequestBody User user){
+    public List<User> putUser(@PathVariable int id, @RequestBody User user) {
         User findUser = users.stream().filter(user1 -> user1.getId() == id).findFirst().get();
         int indexOf = users.indexOf(findUser);
         users.set(indexOf, user);
@@ -73,33 +72,48 @@ public class MainController {
     }
 
     @DeleteMapping("/users/{id}")
-    public List<User> deleteUser(@PathVariable int id){
+    public List<User> deleteUser(@PathVariable int id) {
         users.removeIf(next -> next.getId() == id);
         return users;
     }
 
     @GetMapping("/posts")
-    public List<Post> posts(){
+    public List<Post> posts() {
+        System.out.println(posts);
         return posts;
     }
 
     @GetMapping("/posts/{id}")
-    public Post post(@PathVariable("id") int id){
-        return posts.stream().filter(post -> post.getId()==id).findFirst().get();
+    public Post post(@PathVariable("id") int id) {
+        return posts.stream().filter(post -> post.getId() == id).findFirst().get();
     }
 
-    @GetMapping("/posts")
-    public Post getPOstById(@RequestParam("postID") int postID){
-        return posts.stream().filter(post -> post.getId()==postID).findFirst().get();
+    @GetMapping("/post")
+    public Post getPostByParam(@RequestParam("postId") int postId) {
+        return posts.stream().filter(post -> post.getId() == postId).findFirst().get();
     }
 
-    @PostMapping("/posts")
-    public List<Post> savePost(@RequestBody Post post){
-        if (post!= null){
+    @PostMapping("/post")
+    public List<Post> savePost(@RequestBody Post post) {
+        if (post != null) {
             posts.add(post);
-        }else {
+        } else {
             throw new RuntimeException("Request Body is empty");
         }
+        return posts;
+    }
+
+    @PutMapping("/posts/{id}")
+    public List<Post> updatePost(@PathVariable("id") int id, @RequestBody Post post) {
+        Post findPost = posts.stream().filter(post1 -> post1.getId() == id).findFirst().get();
+        int indexOf = posts.indexOf(findPost);
+        posts.set(indexOf, post);
+        return posts;
+    }
+
+    @DeleteMapping("/posts/{id}")
+    public List<Post> deletePost(@PathVariable("id") int id) {
+        posts.removeIf(next -> next.getId() == id);
         return posts;
     }
 
